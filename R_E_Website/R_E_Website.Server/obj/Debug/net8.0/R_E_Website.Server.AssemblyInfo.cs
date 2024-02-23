b66ae57a38f4f8fc00ab76cf743195aab520bbc1
DaniilBesaga <daniilbesaga@gmail.com>
2024-02-23 12:06:21 +0200

    Add header and some content within

--- R_E_Website/R_E_Website.Server/obj/Debug/net8.0/R_E_Website.Server.AssemblyInfo.cs
+++ R_E_Website/R_E_Website.Server/obj/Debug/net8.0/R_E_Website.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("R_E_Website.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc0ae33e6352ea987b6ce85f1386b952e9645c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("R_E_Website.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("R_E_Website.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
