87c998fd0d0fd7b1d1d779badfb911d7c44e3fbe
DaniilBesaga <daniilbesaga@gmail.com>
2024-02-24 08:36:30 +0200

    Add the slideshow file

--- R_E_Website/r_e_website.client/src/main.tsx
+++ R_E_Website/r_e_website.client/src/main.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.tsx'
+import SlideShow from './components/SlideShow.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Header></Header>
+    <div>
+        <Header></Header>
+        <SlideShow></SlideShow>
+    </div>
 )
