7ff5600674d70e570754251439c5ca97c777a9e9
Даниил Бесага <daniilbesaga@gmail.com>
2024-02-24 08:36:30 +0200

    Add the slideshow file

---
+++ R_E_Website/r_e_website.client/src/components/SlideShow.tsx
@@ -0,0 +1,15 @@
+﻿
+
+function SlideShow() {
+    return (
+        <div>
+            <div className="item" style={{backgroundImage: 'url(https://profirealt.com.ua/wp-content/uploads/2021/01/prodazh_neruhomosti-scaled.jpg)'}}>
+                <div>
+                    Продаж нерухомості
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SlideShow;
