02f2afbd20a78dd92f574ed7dab106d4f586eb29
DaniilBesaga <daniilbesaga@gmail.com>
2024-02-25 21:35:11 +0200

    Add inst posts page

--- R_E_Website/r_e_website.client/src/main.tsx
+++ R_E_Website/r_e_website.client/src/main.tsx
@@ -5,10 +5,11 @@ import Header from './components/Header.tsx'
 import SlideShow from './components/SlideShow.tsx'
 import SellAd from './components/SellAd.tsx'
 import Reviews from './components/Reviews.tsx'
+import InstPosts from './components/InstPosts.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <div>
         <Header></Header>
-        <Reviews></Reviews>
+        <InstPosts></InstPosts>
     </div>
 )
