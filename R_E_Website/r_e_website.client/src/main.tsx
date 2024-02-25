3bd8b76e146003480a56aedc2b2c0288c2cb031b
DaniilBesaga <daniilbesaga@gmail.com>
2024-02-25 13:00:03 +0200

    Add reviews component

--- R_E_Website/r_e_website.client/src/main.tsx
+++ R_E_Website/r_e_website.client/src/main.tsx
@@ -4,10 +4,11 @@ import './index.css'
 import Header from './components/Header.tsx'
 import SlideShow from './components/SlideShow.tsx'
 import SellAd from './components/SellAd.tsx'
+import Reviews from './components/Reviews.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <div>
         <Header></Header>
-        <SellAd></SellAd>
+        <Reviews></Reviews>
     </div>
 )
