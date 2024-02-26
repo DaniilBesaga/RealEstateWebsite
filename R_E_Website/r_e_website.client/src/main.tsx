07387a7cab4a47f8a171f119a6082c728031f7bf
Даниил Бесага <daniilbesaga@gmail.com>
2024-02-26 21:39:35 +0200

    Add a footer

--- R_E_Website/r_e_website.client/src/main.tsx
+++ R_E_Website/r_e_website.client/src/main.tsx
@@ -6,10 +6,11 @@ import SlideShow from './components/SlideShow.tsx'
 import SellAd from './components/SellAd.tsx'
 import Reviews from './components/Reviews.tsx'
 import InstPosts from './components/InstPosts.tsx'
+import Footer from './components/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <div>
         <Header></Header>
-        <InstPosts></InstPosts>
+        <Footer></Footer>
     </div>
 )
