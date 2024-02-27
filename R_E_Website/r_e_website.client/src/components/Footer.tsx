975254f0c81a9b68a18a7591b77ed74319119777
Даниил Бесага <daniilbesaga@gmail.com>
2024-02-27 14:32:26 +0200

    Finished up the footer

--- R_E_Website/r_e_website.client/src/components/Footer.tsx
+++ R_E_Website/r_e_website.client/src/components/Footer.tsx
@@ -66,9 +66,78 @@ function Footer() {
                 <div className="footer-info-part-2">
                     <ul>
                         <li>
-                            <ul></ul>
+                            <ul className="menu-item">
+                                <li><a>Квартири</a></li>
+                                <li><a>Будинки</a></li>
+                                <li><a>Земля</a></li>
+                                <li><a>Комерційна нерухомість</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul className="menu-item">
+                                <li><span><b>Послуги</b></span></li>
+                                <li><a>Купуємо</a></li>
+                                <li><a>Консультуємо</a></li>
+                                <li><a>Міняємо</a></li>
+                                <li><a>Допомагаємо</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul className="menu-item">
+                                <li style={{ opacity:0 }}><span><b>Послуги</b></span></li>
+                                <li><a>Продаємо</a></li>
+                                <li><a>Інвестуємо</a></li>
+                                <li><a>Кредитуємо</a></li>
+                                <li><a>Корисна інформація</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <ul className="menu-item">
+                                <li><span><b>Про компанію</b></span></li>
+                                <li><a>Контакти</a></li>
+                                <li><a>Про нас</a></li>
+                                <li><a>Вакансії</a></li>
+                                <li><a>Відгуки</a></li>
+                            </ul>
                         </li>
                     </ul>
+                </div>
+            </div>
+
+            <div className="section-other">
+                <div className="section-other-part-1">
+                    <div className="lang-switch">
+                        <span>мова:</span>
+                        <span>
+                            <a className="active-link">Укр</a>
+                            <a>Ру</a>
+                        </span>
+                    </div>
+                    <div className="soc-media">
+                        <p>Будьте на зв'язку</p>
+                        <div>
+                            <a><i className="ri-facebook-box-line"></i></a>
+                            <a><i className="ri-instagram-line"></i></a>
+                        </div>
+                    </div>
+                    <div className="partners">
+                        <a href="https://address.ua/" target="_blank">
+                            <img src="https://profirealt.com.ua/wp-content/uploads/2020/07/nfgtelkh.png"></img>
+                        </a>
+                        <a href="https://www.country.ua/" target="_blank">
+                            <img src="https://profirealt.com.ua/wp-content/uploads/2020/07/kc5o5ciy.png" />
+                        </a>
+                        <a href="https://flatfy.ua/uk/нерухомість-київська-область" target="_blank">
+                            <img src="https://profirealt.com.ua/wp-content/uploads/2020/09/Logo_black_tr-2x.png" />
+                        </a>
+                        <a href="https://domik.ua/" target="_blank">
+                            <img src="https://profirealt.com.ua/wp-content/uploads/2020/07/9qdwl2z4.png" />
+                        </a>
+
+                    </div>
+                </div>
+                <div className="section-other-part-2">
+                    <p>ProfiRealt © 2019 - 2024, <a>Угода користувача</a></p>
                 </div>
             </div>
 
