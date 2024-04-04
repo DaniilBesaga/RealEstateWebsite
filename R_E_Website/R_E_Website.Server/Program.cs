17150a807a96c53eb5bc8f992d57e3e4ec8e0b0d
DaniilBesaga <daniilbesaga@gmail.com>
2024-04-04 09:43:03 +0300

    Add services

--- R_E_Website/R_E_Website.Server/Program.cs
+++ R_E_Website/R_E_Website.Server/Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using R_E_Website.Server.Data;
+using R_E_Website.Server.GenericRepository;
+using R_E_Website.Server.Interfaces;
+using R_E_Website.Server.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IComplexDTORepository, ComplexDTORepository>();
+builder.Services.AddScoped<IEstateDTORepository, EstateDTORepository>();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 var app = builder.Build();
 
 app.UseDefaultFiles();
